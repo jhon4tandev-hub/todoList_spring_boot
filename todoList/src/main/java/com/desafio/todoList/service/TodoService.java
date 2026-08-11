a4ddeee19b8c90186876ad960945ed277c548d08
Jhonatan Silva <jhon4ta.n.dev@gmail.com>
2026-08-11 19:49:02 -0300

    feat: foi implementada a camada de servico Todoservice em ordenacao

---
+++ todoList/src/main/java/com/desafio/todoList/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.desafio.todoList.service;
+
+import com.desafio.todoList.entity.Todo;
+import com.desafio.todoList.repository.TodoRepository;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private final TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    private List list() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "prioridade")
+                .and(Sort.by(Sort.Direction.ASC, "nome"));
+
+        return todoRepository.findAll(sort);
+    }
+
+    public List create(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List update(Todo todo) {
+        todoRepository.save(todo);
+        return list();
+    }
+
+    public List delete(Long id) {
+        todoRepository.deleteById(id);
+        return list();
+    }
+}
